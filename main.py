958f20383b72c56d0f0af62c79ed54018c98dd68
Hatice Kübra Babaoğlu <100375578+haticebabaoglu@users.noreply.github.com>
2023-02-13 02:51:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-myName = "hatice"
-myLastName = "babaoglu"
-print(myName + " " +myLastName)
 
-favNum = input("bir sayi giriniz")
-favNum2  =input("tekrar sayi girin")
+Num = input("bir sayi giriniz")
+Num2  =input("tekrar sayi girin")
 
 print(str(favNum) + "+" + str(favNum2) + "=" + str (favNum + favNum2))
