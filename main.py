c6dbef724d533bdb04b18a286a61300d7bc93ae7
Hatice Kübra Babaoğlu <100375578+haticebabaoglu@users.noreply.github.com>
2023-02-13 02:50:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+myName = "hatice"
+myLastName = "babaoglu"
+print(myName + " " +myLastName)
+
+favNum = input("bir sayi giriniz")
+favNum2  =input("tekrar sayi girin")
+
+print(str(favNum) + "+" + str(favNum2) + "=" + str (favNum + favNum2))
