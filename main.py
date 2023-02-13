9156456c74489179f780b2c15498d881d37dc505
Hatice Kübra Babaoğlu <100375578+haticebabaoglu@users.noreply.github.com>
2023-02-13 03:12:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+myName = "hatice"
+myLastName = "babaoglu"
+print(myName + " " +myLastName)
+
+Num = input("Bir sayi giriniz: ")
+Num2  =input("Tekrar bir sayi girin: ")
+
+print(str(Num) + "+" + str(Num2) + "=" + str (Num + Num2))
+
+vize = float(input("Vize notu giriniz: "))* 0.4
+final = float(input("Final notu giriniz: "))*0.6
+
+print(vize + final)
+
+
